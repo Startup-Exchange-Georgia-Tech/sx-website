f3fd1c01d4867613e23b1385f2e0f571ac8ae0c9
Yatharth Bhargava <yatharthbhargava@gmail.com>
2022-08-27 18:08:55 -0400

    footer links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -14,18 +14,24 @@ const Footer = () => {
                     <div className={styles.FooterLogo}>
                         <Image src={secondaryLogo}></Image>
                     </div>
-                    <div className={styles.helloSX}>
-                        <Image src={smallArrow}></Image>
-                        <div className={styles.helloText}><u>hello@startupexchange.com</u></div>
-                    </div>
+                    <Link href="mailto:hello@startup.exchange">
+                        <div className={styles.helloSX}>
+                            <Image src={smallArrow}></Image>
+                            <div className={styles.helloText}><u>hello@startupexchange.com</u></div>
+                        </div>
+                    </Link>
+                    
                 </div>
                 <div className={styles.FooterRight}>
                     <div className={styles.FooterList}>
-                        <div className={styles.helloSX}>
-                            <div className={styles.FooterListHeading}>HOME</div>
-                            <Image src={smallArrow} className={styles.reverseArrow}></Image>
-                        </div>
+                        <Link href="#Hero">
+                            <div className={styles.helloSX}>
+                                <div className={styles.FooterListHeading}>HOME</div>
+                                <Image src={smallArrow} className={styles.reverseArrow}></Image>
+                            </div>
+                        </Link>
                         
+
                         <div className={styles.FooterListItem}>SX TOOLBOX</div>
                         <div className={styles.FooterListItem}>CONTACT</div>
                     </div>
@@ -46,31 +52,36 @@ const Footer = () => {
                             </Link>
 
                         </div>
-                        <div className={styles.helloSX}>
-                            <Image src={smallArrow}></Image>
-                            <div className={styles.FooterListItem}>gatech</div>
-                            
-                        </div>
-                        <div className={styles.helloSX}>
-                            <Image src={smallArrow}></Image>
-                            <div className={styles.FooterListItem}>create-x</div>
-                        </div>
+                        <Link href="https://www.gatech.edu/">
+                            <div className={styles.helloSX}>
+                                <Image src={smallArrow}></Image>
+                                <div className={styles.FooterListItem}>gatech</div>
+                                
+                            </div>
+                        </Link>
+                        <Link href="https://create-x.gatech.edu/">
+                            <div className={styles.helloSX}>
+                                <Image src={smallArrow}></Image>
+                                <div className={styles.FooterListItem}>create-x</div>
+                            </div>
+                        </Link>
+                        
                     </div>
                     <div className={styles.FooterList}>
                         <Link href="#AboutUs">
-                            <div className={styles.FooterListHeading}>ABOUT</div>
+                            <div className={styles.FooterListLink}>ABOUT</div>
                         </Link>
                         <Link href="#Join">
-                            <div className={styles.FooterListHeading}>JOIN</div>
+                            <div className={styles.FooterListLink}>JOIN</div>
                         </Link>
                         <Link href="#Events">
-                            <div className={styles.FooterListHeading}>EVENTS</div>
+                            <div className={styles.FooterListLink}>EVENTS</div>
                         </Link>
                         <Link href="#Alumni">
-                            <div className={styles.FooterListHeading}>ALUMNI</div>
+                            <div className={styles.FooterListLink}>ALUMNI</div>
                         </Link>
                         <Link href="#Exec">
-                            <div className={styles.FooterListHeading}>EXEC</div>
+                            <div className={styles.FooterListLink}>EXEC</div>
                         </Link>
                     </div>
                 </div>
