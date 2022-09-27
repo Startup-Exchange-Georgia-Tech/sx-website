5f533522ab9525bb61136a6abb65bcdef90d54c9
CarterCote <cartercote06@gmail.com>
2022-09-26 21:10:40 -0400

    updated mission statement

--- components/AboutUs/AboutUs.js
+++ components/AboutUs/AboutUs.js
@@ -10,9 +10,9 @@ export default function AboutUs() {
             <div className={styles.AboutUsLeft}>
                 <div className={styles.AboutUsText}>ABOUT US</div>
                 <div className={styles.AboutUsHeading}>From Concept to Prototype</div>
-                <div className={styles.AboutUsBody}>Startup Exchange is the largest entrepreneurship community at Georgia Tech. 
+                <div className={styles.AboutUsBody}>Startup Exchange is the largest entrepreneurship organization at Georgia Tech. 
                     Our mission is to inspire our members to create and develop their ventures by cultivating a hub for members to <u> connect</u>, <u>collaborate</u>, and <u>innovate</u>.<br></br><br></br>
-                    Through shared experiences and collective learning, our professional network and curriculum empowers students at all levels of entrepreneurship 
+                    Through our network of founders and curriculum taught by industry leaders, SX empowers students at all levels of entrepreneurship 
                     to develop the literacy to build their own ventures.</div>
                 <Link href="#Join">
                     <div className={styles.eventLink}>
