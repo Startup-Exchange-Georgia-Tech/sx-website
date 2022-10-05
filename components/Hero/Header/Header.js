7851752a52041c1a86f06c869108f812a33e3071
CarterCote <cartercote06@gmail.com>
2022-10-04 23:32:02 -0400

    Added Members-Only button

--- components/Hero/Header/Header.js
+++ components/Hero/Header/Header.js
@@ -34,8 +34,9 @@ export default function Header() {
                     <Link href="/#Join">
                         <div className={styles.ddLink}>Join</div>
                     </Link>
-                    
-                    <div className={styles.ddLink}>Learn</div>
+                    <Link href="https://startupexchange.notion.site/SX-Toolbox-658b9ef658da4ba7bb8d4cbdc470a387">
+                        <div className={styles.ddLink}>Learn</div>
+                    </Link>
                     <Link href="/contact">
                         <div className={styles.ddLinkContact}>Contact</div>
                     </Link>
@@ -57,14 +58,14 @@ export default function Header() {
                     <Link href="/#Join">
                         <a className={styles.headerPageLink}>JOIN</a>
                     </Link>
-                    <Link href="/#Events">
+                    <Link href="https://startupexchange.notion.site/SX-Toolbox-658b9ef658da4ba7bb8d4cbdc470a387">
                         <a className={styles.headerPageLink}>LEARN</a>
                     </Link>
                     <Link href="/contact">
                         <a className={styles.headerPageLink}>CONTACT</a>
                     </Link>
-                    <Link href="https://startupexchange.notion.site/SX-Toolbox-658b9ef658da4ba7bb8d4cbdc470a387">
-                    <a className={styles.headerPageLink}><span className={styles.yellow}><u>SX TOOLBOX</u></span></a>
+                    <Link href="https://www.google.com/search?q=coming+soon&rlz=1C1RXQR_enUS970US970&sxsrf=ALiCzsZfqKcJzQ7lZqpnXi44y7yB0Vmowg:1664940615514&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjLp9aPk8j6AhW5mGoFHWlxAAoQ_AUoAXoECAIQAw&biw=1920&bih=929&dpr=17">
+                    <a className={styles.headerPageLink}><span className={styles.yellow}><u>MEMBERS ONLY Hub</u></span></a>
                     </Link>
                 </div>
                 <div className={styles.pagesDropDown}>
